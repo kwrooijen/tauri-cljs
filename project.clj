e7329a03122076d18ca2f5dbcc32973e1cabd591
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-05-15 23:03:49 +0200

    Fix license conflict

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject tauri-cljs/lein-template "0.0.1"
   :description "A leiningen template for Shadow-cljs + Tauri"
   :url "https://github.com/kwrooijen/tauri-cljs"
-  :license {:name "MIT"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"}
   :eval-in-leiningen true)
