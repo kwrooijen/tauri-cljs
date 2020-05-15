7b059108da593f016e93b5aea9caa2e873c7ca4b
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-05-15 18:18:34 +0200

    Add LICENSE

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tauri-cljs/lein-template "0.0.0"
+(defproject tauri-cljs/lein-template "0.0.1"
   :description "A leiningen template for Shadow-cljs + Tauri"
   :url "https://github.com/kwrooijen/tauri-cljs"
-  :license {:name "EPL-2.0"
+  :license {:name "MIT"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :eval-in-leiningen true)
