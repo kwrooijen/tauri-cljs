b0f1a7fd4052f9e7d40bc6671348e7dc3bdd6a04
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-05-15 23:15:36 +0200

    Add :deploy-repositories key

--- project.clj
+++ project.clj
@@ -2,4 +2,9 @@
   :description "A leiningen template for Shadow-cljs + Tauri"
   :url "https://github.com/kwrooijen/tauri-cljs"
   :license {:name "MIT"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                    :username :env/clojars_user
+                                    :password :env/clojars_pass
+                                    :sign-releases false}]])
