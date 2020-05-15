8ae6993227fc7f2c9b1b13dd81338160a059118c
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-05-15 23:04:26 +0200

    RELEASE 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tauri-cljs/lein-template "0.0.1"
+(defproject tauri-cljs/lein-template "0.0.2"
   :description "A leiningen template for Shadow-cljs + Tauri"
   :url "https://github.com/kwrooijen/tauri-cljs"
   :license {:name "MIT"}
